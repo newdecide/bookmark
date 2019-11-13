c138c1963e37c6116f6cc5f57dd30419939ecc60
newdecide <liberty-1@nate.com>
2019-11-13 15:55:17 +0900

    배포 설정 완료

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6zgmd_)9yevj+j5+r=z*bm)qsg&s68-z550u-r%kb!u+$(_nq6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
